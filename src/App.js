8dbbfa21f968f5b778561ad05b8d5b69920102ab
Prasad Hadbe <Prasadahadbe313@gmail.com>
2023-06-02 20:03:22 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React, { useState } from 'react'
-import Menu from './Menu'
-import Categories from './Categories'
-import items from './data'
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
+  const categories = [...new Set(items.map((item) => item.category))];
 
-const categories = [...new Set(items.map((item)=> item.category))];
+  const [menuItems, setMenuItems] = useState(items);
 
-	const [menuItems, setMenuItems] = useState(items);
+  const filterItems = (category) => {
+    console.log(category);
+    if (category === "all") {
+      setMenuItems(items);
+    } else {
+      setMenuItems(() => items.filter((item) => item.category === category));
+    }
+  };
 
-	const filterItems = (category) => {
-		console.log(category);
-		if(category === 'all'){
-			setMenuItems(items)
-		}else {
-			setMenuItems(()=> items.filter(item => item.category === category));
-		}
-	}
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
 
-	return(
-	<main>
-		<section className='menu section'>
-			<div className='title'>
-				<h2>our menu</h2>
-				<div className='underline'></div>
-			</div>
-			<Categories categories={categories} filterItems={filterItems}/>
-			
-			<Menu items={menuItems}/>
-		</section>
-	</main>)
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
